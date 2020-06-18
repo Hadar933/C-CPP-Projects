e2ad5d11f6cc3c841a6439831b59757fc0fc90a3
Hadar Sharvit <hadar.sharvit1@mail.huji.ac.il>
2020-06-18 22:53:33 +0300

    finished some file parsing
    movie titles are now represented as a vector

--- Ex5/RecommenderSystem.cpp
+++ Ex5/RecommenderSystem.cpp
@@ -1,10 +1,61 @@
-#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include "RecommenderSystem.h"
+
+bool gFirstLine = true;
 
 
+bool
+RecommenderSystem::loadData(const string &moviesAttributesFilePath, const string &userRanksFilePath)
+{
+}
 
+bool RecommenderSystem::parseFile(const string &filePath, bool isUserRank)
+{
+	std::ifstream file;
+	file.open(filePath);
+	int fileSize = _getFileSize(file);
+	string line;
+	while (std::getline(file, line))
+	{
+		if (gFirstLine && isUserRank)
+		{ // first line represents movies names (only in user ranks file)
+			parseEachLine(line,_movieNamesVec);
+			gFirstLine = false;
+		}
+		else
+		{
+		}
+
+	}
+}
+
+
+int RecommenderSystem::_getFileSize(std::ifstream &file)
+{
+	file.seekg(0, std::ios::end);
+	std::streampos length = file.tellg();
+	file.seekg(0, std::ios::beg);
+	return length;
+
+}
+
+void RecommenderSystem::parseEachLine(string &line, std::vector<string> &vec)
+{
+	std::istringstream iss(line);
+	do
+	{
+		// reading the word
+		string word;
+		iss >> word;
+		//adding the word to a provided vector
+		vec.push_back(word);
+	} while (iss); // repeat for all words in line
+
+}
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
+	RecommenderSystem obj;
+	obj.parseFile("ranks_matrix.txt",true);
 }
--- Ex5/RecommenderSystem.h
+++ Ex5/RecommenderSystem.h
@@ -5,9 +5,81 @@
 #ifndef EX5_RECOMMENDERSYSTEM_H
 #define EX5_RECOMMENDERSYSTEM_H
 
+#include <string>
+#include <iostream>
+#include <vector>
 
+#define SUCCESS 0
+#define FAILURE 1
+#define INVALID -1
+#define BAD_PATH "Unable to open file "
+#define BAD_USER "USER NOT FOUND"
+
+using std::string;
+using std::cerr;
+using std::endl;
+
+/**
+ *based on movie attributes file and a user ranks file, the class is responsible
+ * of generating relevant recommendations to the user, in multiple ways.
+ */
 class RecommenderSystem
 {
+public:
+	/**
+	 * loads the data to our recommendation system
+	 * @param moviesAttributesFilePath - filepath to movies attributes matrix
+	 * @param userRanksFilePath - filepath to user ranks matrix
+	 * @return 0 upon success, 1 otherwise.
+	 */
+	static bool loadData(const string &moviesAttributesFilePath, const string &userRanksFilePath);
+
+	/**
+	 * parses one of the files
+	 * @param filePath - path to file to parse
+	 * @param isUserRank - represents that the file is the user ranks
+	 * @return
+	 */
+	bool parseFile(const string &filePath, bool isUserRank);
+
+	bool parseFeatures(const string &moviesAttributesFilePath);
+
+	/**
+	 *generates a recommendation based on the content
+	 * @param username - customer's user name
+	 * @return string - representing the recommended movie returned by the algorithm
+	 */
+	std::string recommendByContent(char *username);
+
+	/**
+	 * generates a prediction for a user's rank to a movie he hasn't seen yet, based
+	 * on the movies the user has already seen
+	 * @param movieName - movie to predict a rating for
+	 * @param userName - name of the customer
+	 * @param k - number of movies that are most similar to movieName
+	 * @return -1 if no such user or movie, float predicted rating otherwize
+	 */
+	double predictMovieScoreForUser(string movieName, string userName, int k);
+
+	/**
+	 * recommends a movie to the user based on the predictMovieScoreForUser algorithm
+	 * @param userName - name of the customer
+	 * @param k - number of movies that are most similar to movieName
+	 * @return string representing the name of the recommended movie
+	 */
+	std::string recommendByCF(string userName, int k);
+
+private:
+	/**
+	 * returns a file's size (assume file exists)
+	 * @param file some file objecy
+	 * @return file's size
+	 */
+	static int _getFileSize(std::ifstream &file);
+
+	static void parseEachLine(string &line, std::vector<string> &vec);
+
+	std::vector<string> _movieNamesVec;
 
 };
 
