13700eba3bd4e1970dfea441f250eda15ed78cdc
Hadar Sharvit <hadar.sharvit1@mail.huji.ac.il>
2020-06-18 17:03:00 +0300

    started proj 5

---
+++ Ex5/RecommenderSystem.h
@@ -0,0 +1,15 @@
+//
+// Created by hadar on 18-Jun-20.
+//
+
+#ifndef EX5_RECOMMENDERSYSTEM_H
+#define EX5_RECOMMENDERSYSTEM_H
+
+
+class RecommenderSystem
+{
+
+};
+
+
+#endif //EX5_RECOMMENDERSYSTEM_H
---
+++ Ex5/cmake-build-debug/CMakeFiles/Ex5.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ex5.dir/main.cpp.o"
+  "Ex5.exe"
+  "Ex5.pdb"
+  "libEx5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ex5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
