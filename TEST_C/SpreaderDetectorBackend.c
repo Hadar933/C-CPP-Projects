b133011ac494cbe2ca48ba4e20da7b3bc6934a88
Hadar Sharvit <hadar.sharvit1@mail.huji.ac.il>
2020-08-02 14:41:58 +0300

    parsing people.in
    finished writing. not tested

--- TEST_C/SpreaderDetectorBackend.c
+++ TEST_C/SpreaderDetectorBackend.c
@@ -11,8 +11,6 @@
 int gNumOfNodes = 0; //global value representing the number of lines in people.in, which
 // corresponds to nodes in the graph data structure
 
-//FILE *gPeopleFile;
-//FILE *gMeetingFile;
 
 /**
  * stores the data of each person (each line in people.in)
@@ -36,24 +34,31 @@ typedef struct Edge
 	float time, dist; // from source to dest
 } Edge;
 
-typedef struct Graph
+/**
+ * when allocation fails, this function is being called to free all memory, close all files
+ * and output an error message
+ * @param file - some file that should to be closed
+ * @param array - some array that should be freed
+ */
+void allocationExitProtocol(FILE** file, Node** array)
 {
-
-} Graph;
-
+	fprintf(stderr, INPUT_ERROR);
+	fclose(*file);
+	free(array);
+	exit(EXIT_FAILURE);
+}
 /**
  * extracts a person data from a line given from people.in
  * @param line - some line in the format <Person name> <Person ID> <Person age>\n
  */
-void parsePersonLine(char line[])
+void parsePersonLine(char line[], Node **peopleArray)
 {
 	char personName[MAX_LINE_LEN], charID[MAX_LINE_LEN], charAge[MAX_LINE_LEN];
-	float personAge = 0;
-	int personID =0;
 	sscanf(line, "%[^ ], %[^ ], %[^\n]\n", personName, charID, charAge);
-	strtof(charAge, (char **) &personAge); //converts the age to float value
-	strtof(charID, (char **) &personID); //converts the ID to int value
-	Node person = {.name=*personName,.age=personAge,.ID=*charID,.next=NULL};
+	float personAge = strtof(charAge, NULL); //converts the age to float value
+	int personID = (int) strtof(charID, NULL); //converts the ID to int value
+	Node person = {.name=*personName, .age=personAge, .ID=personID, .next=NULL};
+	peopleArray[gNumOfNodes] = &person;
 }
 
 /**
@@ -62,20 +67,38 @@ void parsePersonLine(char line[])
  */
 void readPeopleFile(char const *const peopleFileName)
 {
-	FILE *peopleFile = fopen(peopleFileName, "r");
-	// CHECKING VALID INPUT: no such file or file is empty
-	if (ftell(peopleFile) == END_FILE || peopleFileName == NULL)
+	// Memory Allocation:
+	int size = ALLOC_SIZE;
+	Node *peopleArray = (Node *) malloc(size*sizeof(Node)); // representing all data in people.in
+	if(peopleArray==NULL) // allocation failed
 	{
-		fprintf(stderr, INPUT_ERROR);
-		fclose(peopleFile);
+		fprintf(stderr,STANDARD_LIB_ERR_MSG);
 		exit(EXIT_FAILURE);
 	}
+	// CHECKING VALID INPUT:
+	FILE *peopleFile = fopen(peopleFileName, "r");
+	if (ftell(peopleFile) == END_FILE || peopleFileName == NULL) //no such file or file is empty
+	{
+		allocationExitProtocol(&peopleFile,&peopleArray);
+	}
 	fseek(peopleFile, END_FILE, SEEK_SET); // file exists and isn't empty - return to the beginning
+
+	// Parsing Data:
 	char personLine[MAX_LINE_LEN];
 	while (fgets(personLine, (int) sizeof(personLine), peopleFile)) // each line is some person
 	{
+		if(size==gNumOfNodes) // this means we don't have enough space to allocate -> get some more
+		{
+			size+=ALLOC_SIZE;
+			peopleArray = (Node*) realloc(peopleArray,size*sizeof(Node));
+			if (peopleArray==NULL) //reallocation failed
+			{
+				allocationExitProtocol(&peopleFile,&peopleArray);
+			}
+		}
+		// we are now able to parse the line properly:
+		parsePersonLine(personLine, &peopleArray);
 		gNumOfNodes++;
-		parsePersonLine(personLine);
 	}
 
 
