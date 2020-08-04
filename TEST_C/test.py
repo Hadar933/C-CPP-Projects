409db6f255671e93c1ed90b589d5676d0cd0b9f8
Hadar Sharvit <hadar.sharvit1@mail.huji.ac.il>
2020-08-04 12:54:02 +0300

    finished

--- TEST_C/test.py
+++ TEST_C/test.py
@@ -1,15 +1,16 @@
 import random, string, os
 
 NUMBER_OF_FILES = 10
-PEOPLE_NUM = 100
-MEETINGS_NUM = 50
+PEOPLE_NUM = 50000
+MEETINGS_NUM = 30000
 
 
 def gen_files():
 	for i in range(NUMBER_OF_FILES):
+		print("creating file number:"+str(i))
 		people = list()
-		pathPeople = 'in-out-example/' + str(i + 4) + '_people.in'
-		pathMeeting = 'in-out-example/' + str(i + 4) + '_meeting.in'
+		pathPeople = 'in-out-example/' + str(i + 4) + '_people(big).in'
+		pathMeeting = 'in-out-example/' + str(i + 4) + '_meeting(big).in'
 		# people file
 		with open(pathPeople, 'w') as peopleFile:
 			for j in range(PEOPLE_NUM):
@@ -39,27 +40,33 @@ def gen_files():
 					line = "{0:d} {1:d} {2:f} {3:f}\n".format(spreader, infected, dist, time)
 					meetingFile.write(line)
 					count += 1
+		print("finished creating file number: "+str(i))
 
 
 def run_code():
-	for i in range(1, NUMBER_OF_FILES + 4):
-		run_string = '"cmake-build-debug\\SpreaderDetector.exe in-out-example\\{0}_people.in in-out-example\\{0}_meeting.in"'.format(i)
+	for i in range(4, NUMBER_OF_FILES + 4):
+		print("running file "+str(i))
+		run_string = '"cmake-build-debug\\SpreaderDetector.exe in-out-example\\{0}_people(big).in in-out-example\\{0}_meeting(big).in"'.format(i)
 		os.system(run_string)
-		sol_str = "in-out-example\\{0}_sol".format(i)
+		sol_str = "in-out-example\\{0}_sol(big)".format(i)
 		save_str = "cat SpreaderDetectorAnalysis.out > " + sol_str + '.out'
 		os.system(save_str)
-		diff_str = "FC " + sol_str + '.out ' + sol_str + '.cmp > ' + sol_str + '.diff'
-		os.system(diff_str)
-		diff_name = sol_str + '.diff'
-		with open(diff_name) as diff_file:
-			if not diff_file.readlines()[1].__contains__("no differences encountered"):
-				print('error in file number: ' + str(i))
-			else:
-				os.remove(diff_name)
-	run_string = '"cmake-build-debug\\SpreaderDetector.exe in-out-example\\empty in-out-example\\empty"'
-	os.system(run_string)
-	if os.path.getsize("../SpreaderDetectorAnalysis.out") != 0:
-		print('error with empty files')
+		print("finished running file num "+str(i))
+	# 	diff_str = "FC " + sol_str + '.out ' + sol_str + '.cmp > ' + sol_str + '.diff'
+	# 	os.system(diff_str)
+	# 	diff_name = sol_str + '.diff'
+	# 	with open(diff_name) as diff_file:
+	# 		if not diff_file.readlines()[1].__contains__("no differences encountered"):
+	# 			print('error in file number: ' + str(i))
+	# 		else:
+	# 			diff_file.close()
+	# 			print("passed test:"+str(i))
+	# 			os.remove(diff_name)
+	# run_string = '"cmake-build-debug\\SpreaderDetector.exe in-out-example\\empty in-out-example\\empty"'
+	# os.system(run_string)
+	# if os.path.getsize("SpreaderDetectorAnalysis.out") != 0:
+	# 	print('error with empty files')
+	# print('passed empty test')
 
 
 if __name__ == "__main__":
